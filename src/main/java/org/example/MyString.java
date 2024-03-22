b814facdcf004f51d49f723aedd2f02a69cb9475
Balminess <hpf30248@gmail.com>
2024-03-22 18:38:06 -0500

    init

---
+++ src/main/java/org/example/MyString.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class MyString {
+    public int indexOfString(String s1, String s2, int pos) {
+        return -2;
+    };
+}
---
+++ src/test/java/org/example/MyStringTest.java
@@ -0,0 +1,14 @@
+package org.example;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyStringTest  {
+//    @Spy
+    @Test
+    public void testIndexOfString() {
+        MyString myString = new MyString();
+        int result = myString.indexOfString("abcdabad","a",0);
+        assertEquals(0,result);
+
+    }
+}
