3a8da7d143d5e2b3d56dd96e30141639af026797
Balminess <hpf30248@gmail.com>
2024-03-22 21:28:13 -0500

    Making red tests green: search occurrence from pos

--- src/main/java/org/example/MyString.java
+++ src/main/java/org/example/MyString.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class MyString {
     public int indexOfString(String s1, String s2, int pos) {
-        for (int i = 0; i <= s1.length() - s2.length(); i++) {
+        // Looking for the starting index larger than pos
+        for (int i = pos; i <= s1.length() - s2.length(); i++) {
             boolean match = true;
             for (int j = 0; j < s2.length(); j++) {
                 if (s1.charAt(i + j) != s2.charAt(j)) {
