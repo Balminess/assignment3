802f98cfa52913caa37fdd05b317f5a4ad71ed1d
Balminess <hpf30248@gmail.com>
2024-03-23 10:45:39 -0500

    mock the indexOfString method by Mockito, use spy on MyString for dependency injection and doAnswer to configure the return value

--- src/main/java/org/example/MyString.java
+++ src/main/java/org/example/MyString.java
@@ -24,7 +24,6 @@ public class MyString {
     };
 
     public String replace(String s, String s1, String s2) {
-
         // Check if any of the strings is null
         if (s == null || s1 == null || s2 == null)
             return null;
