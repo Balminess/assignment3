39d6283f7bbf7ba98771f295d51495bbff15ba70
Balminess <hpf30248@gmail.com>
2024-03-22 19:00:58 -0500

    fix the RED unittest(blank method)

--- src/main/java/org/example/MyString.java
+++ src/main/java/org/example/MyString.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class MyString {
     public int indexOfString(String s1, String s2, int pos) {
-        return -2;
+        for (int i = 0; i <= s1.length() - s2.length(); i++) {
+            boolean match = true;
+            for (int j = 0; j < s2.length(); j++) {
+                if (s1.charAt(i + j) != s2.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return i;
+            }
+        }
+        // No occurrence of s2 found
+        return -1;
     };
 }
